8c34d5ca21b9d8f61dd7b94b8876c37cba30c192
komaliboda <bodakomali12@gmail.com>
2026-03-16 21:24:43 +0530

    Create Relational.py

---
+++ Relational.py
@@ -0,0 +1,9 @@
+#Relational operator
+a=2
+b=4
+print("greaterthan:" ,a>b)
+print("lessthan:" ,a<b)
+print("lessthan is equal to:",a<=b)
+print("gretar than is equal to:",a>=b)
+print(" equal to:",a==b)
+print(" not equal to:",a!=b)
