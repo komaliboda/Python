1e9b16c74ef95e274ae058eb2706da9125947d72
komaliboda <bodakomali12@gmail.com>
2026-04-02 18:53:30 +0530

    Create Simple_calculator.py

---
+++ Simple_calculator.py
@@ -0,0 +1,21 @@
+#--------------------------------------
+#project : Simple calculator 
+#discription : Performs basic operation
+#---------------------------------------
+num1=int(input("enter num1 : "))
+num2=int(input("enter num2 : "))
+addition = num1+num2
+substraction = num1-num2
+multiplication = num1*num2
+division = num1/num2
+exponent = num1**num2
+floor_division=num1//num2
+
+print( "addition:", addition)
+print( "substraction:", substraction)
+print( "multiplication:", multiplication)
+print( "Division:", division)
+print( "power:", exponent)
+print( "Floor division: ", floor_division)
+
+
