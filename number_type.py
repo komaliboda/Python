080b61808e2b43045ed71349d7240a538d35e71f
komaliboda <bodakomali12@gmail.com>
2026-04-02 19:23:09 +0530

    Create number_type.py

---
+++ number_type.py
@@ -0,0 +1,13 @@
+#--------------------------------------
+#project : positive, negative, zero
+#description :✅ check number type 
+#---------------------------------------
+
+num = int(input("Enter num : "))
+
+if num>0:
+    print(" number is positive ")
+elif num<0:
+    print(" number is negative ")
+else:
+    print(" number is zero ")
