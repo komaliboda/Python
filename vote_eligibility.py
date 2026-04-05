8fdd0fb0e16b0fef638e129166c89a482304c4c6
Komali Boda <bodakomali12@gmail.com>
2026-04-05 21:38:11 +0530

    Create vote_eligibility.py

---
+++ vote_eligibility.py
@@ -0,0 +1,14 @@
+#--------------------------------------
+#project : votes eligibility 
+#Description : checks eligibility of vote based on our age
+#--------------------------------------
+
+age = int(input("Enter your age : "))
+
+if age < 0 or age > 100:
+    print("Invalid age")
+elif age >= 18:
+    print("you are Eligible to vote")
+else:
+    print("you are not Eligible to vote")
+    
