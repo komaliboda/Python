55960025da73339decd52c4c5e3e98c79c13a3e4
Komali Boda <bodakomali12@gmail.com>
2026-04-09 18:56:23 +0530

    Create loops_practice.py

---
+++ loops_practice.py
@@ -0,0 +1,26 @@
+#--------------------------------------
+#project : loops practice 
+#description : different loop examples 
+#--------------------------------------
+
+# print 1 to 10
+print("numbers from  1 to 10")
+for i in range(1,11):
+    print(i,end=" ")
+
+#print 10 to 1
+print("\nnumbers from 10 to 1 ")
+for i in range(10,0,-1):
+    print(i,end=" ")
+    
+#print only even numbers 
+print(" \nEven numbers ")
+for i in range(2,20,2):
+    print(i,end=" ")
+    
+#print only odd numbers 
+print("\nodd numbers ")
+for i in range(1,20,2):
+    print(i,end=" ")
+    
+    
