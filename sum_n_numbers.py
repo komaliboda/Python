606f19b590b0b8ee6362b3fac1cf61fcb1fc3b6e
Komali Boda <bodakomali12@gmail.com>
2026-04-09 19:23:42 +0530

    Create sum_n_numbers.py

---
+++ sum_n_numbers.py
@@ -0,0 +1,11 @@
+#--------------------------------------
+#project : sum of numbers
+#description : calculate sum from 1 to 10
+#--------------------------------------
+sum = 0
+for n in range(1,11):
+    sum = sum+n
+print(" The sum is :",sum)
+    
+
+
