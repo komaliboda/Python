0c40704c4304fc6cc9dfe2b39dc9206f3abddc3a
Komali Boda <bodakomali12@gmail.com>
2026-04-19 21:02:33 +0530

    Update pattern_number.py

--- pattern_number.py
+++ pattern_number.py
@@ -39,4 +39,14 @@ for i in range(1,6):
         print(i, end=" ")
     print()
         
-    
+  #pattern 6 : star triangle 
+for i in range(1,6):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+#pattern 7 : reverse star triangle 
+for i in range(5,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()  
