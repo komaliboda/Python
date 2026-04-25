0371f97dce513a0e398f94a42573e722a391a4a7
Komali Boda <bodakomali12@gmail.com>
2026-04-25 19:31:46 +0530

    Update pattern_number.py

--- pattern_number.py
+++ pattern_number.py
@@ -88,3 +88,10 @@ for i in range(5,0,-1):
     for k in range(i):
         print("*",end=" ")
     print()
+
+#pattern 11 : left aligned reverse number triangle 
+
+for i in range(1,6):
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print()
